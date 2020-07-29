f5c54975440ab66a4562c5f4101426ef18697763
Shiraaz Ahammed <shiraaz.ahammed@gmail.com>
2020-07-29 14:44:35 +0530

    Implementation: Side drawer toggle button
    Added Sidedrawer toggle button whenever screen is in mobile width

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import NavigationItems from '../Navigation Items/NavigationItems';
 import Logo from '../../Logo/Logo';
+import DrawerToggle from '../Sidedrawer/DrawerToggle/DrawerToggle'
 import classes from './Toolbar.module.css';
 
 const toolbar = (props) => {
     return (
         <header className={classes.Toolbar}>
+            <DrawerToggle clicked={props.toggleSideDrawer}></DrawerToggle>
             <div className={classes.Logo}>
                 <Logo />
             </div>
-            <nav>
+            <nav className={classes.DesktopOnly}>
                 <NavigationItems />
             </nav>
         </header>
