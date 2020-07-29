719400a230e5498772408742ba6c0612e2b0720b
Shiraaz Ahammed <shiraaz.ahammed@gmail.com>
2020-07-29 23:50:17 +0530

    Addition: Spinner loader for fetching data
    A Spinner loader which stops after api has fetched data

--- src/containers/Content/Content.js
+++ src/containers/Content/Content.js
@@ -1,26 +1,31 @@
 import React, { Component } from 'react';
 import axios from '../../axios-http';
 import Card from '../../components/Card/Card'
+import Spinner from '../../components/UI/Spinner/Spinner'
 import classes from './Content.module.css'
 
 class Content extends Component {
 
     state = {
-        photos: []
+        pizzas: [],
+        loader: true
     }
 
     componentDidMount() {
         axios.get('/search?q=pizza').then(response => {
-            const photos = response.data.recipes.slice(0, 2);
-            console.log(photos)
-            this.setState({photos: photos});
+            const pizzas = response.data.recipes.slice(0, 2);
+            console.log(pizzas)
+            this.setState({pizzas: pizzas, loader: false});
         });
     }
 
     render() {
+
+
         return (
             <div className={classes.Content}>
-                {this.state.photos.map(obj => <Card key={obj.recipe_id} title={obj.title} img={obj.image_url} publisher={obj.publisher}></Card>)}
+                {this.state.pizzas.map(obj => <Card key={obj.recipe_id} title={obj.title} img={obj.image_url} publisher={obj.publisher}></Card>)}
+                {this.state.loader ? <div className={classes.spinner}><Spinner/></div> : null}
             </div>
         )
     }
