2de5b321d8e3815507df10439160bf925d4f48a2
Shiraaz Ahammed <shiraaz.ahammed@gmail.com>
2020-07-30 00:52:23 +0530

    Fix: Changed Nav bar to material ui App bar
    Used material ui app bar and changed the styles and removed unwanted style related code

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,21 +1,58 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
 import NavigationItems from '../Navigation Items/NavigationItems';
 import Logo from '../../Logo/Logo';
-import DrawerToggle from '../Sidedrawer/DrawerToggle/DrawerToggle'
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
 import classes from './Toolbar.module.css';
 
-const toolbar = (props) => {
-    return (
-        <header className={classes.Toolbar}>
-            <DrawerToggle clicked={props.toggleSideDrawer}></DrawerToggle>
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+    ['@media (min-width:500px)']: { // eslint-disable-line no-useless-computed-key
+        display: 'none'
+      }
+  },
+  title: {
+    flexGrow: 1,
+    marginLeft: theme.spacing(2),
+    textAlign: "center",
+  },
+  toolbar: {
+      background: "grey",
+  }
+}));
+
+const ButtonAppBar = (props) => {
+  const materialClasses = useStyles();
+
+  return (
+    <div className={materialClasses.root}>
+      <AppBar position="static" className={materialClasses.toolbar}>
+        <Toolbar>
+            <IconButton edge="start" className={materialClasses.menuButton} color="inherit" aria-label="menu" onClick={props.toggleSideDrawer}>
+                <MenuIcon />
+            </IconButton>
             <div className={classes.Logo}>
                 <Logo />
             </div>
+            <Typography variant="h5" className={materialClasses.title}>
+                Pizza
+            </Typography>
             <nav className={classes.DesktopOnly}>
                 <NavigationItems />
             </nav>
-        </header>
-    )
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
 
-export default toolbar;
+export default ButtonAppBar;
