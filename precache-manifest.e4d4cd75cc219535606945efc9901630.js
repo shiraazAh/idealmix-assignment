6a9152b900bde0f3807452a60ac45695b4199e61
Shiraaz Ahammed <shiraaz.ahammed@gmail.com>
2020-07-30 16:43:48 +0530

    Updates

---
+++ precache-manifest.e4d4cd75cc219535606945efc9901630.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b54ab171026faa122e15d038d9727dc",
+    "url": "/idealmix-assignment/index.html"
+  },
+  {
+    "revision": "fb732353366db70adc56",
+    "url": "/idealmix-assignment/static/css/main.460b93d5.chunk.css"
+  },
+  {
+    "revision": "c8352ea259c387950915",
+    "url": "/idealmix-assignment/static/js/2.8ac1da28.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/idealmix-assignment/static/js/2.8ac1da28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb732353366db70adc56",
+    "url": "/idealmix-assignment/static/js/main.7e96ef2a.chunk.js"
+  },
+  {
+    "revision": "7760bbe39f2631b75a48",
+    "url": "/idealmix-assignment/static/js/runtime-main.3acfe636.js"
+  }
+]);
