c2dc63aee615545e82eab65a1fdd24ee5d23ade4
Shiraaz Ahammed <shiraaz.ahammed@gmail.com>
2020-07-30 13:37:54 +0530

    Addition: errorHandler for content

--- src/containers/Content/Content.js
+++ src/containers/Content/Content.js
@@ -1,35 +1,46 @@
 import React, { Component } from 'react';
-import axios from '../../axios-http';
-import Card from '../../components/Card/Card'
-import Spinner from '../../components/UI/Spinner/Spinner'
+import axios from '../../axios-pizza';
+import Card from '../../components/Card/Card';
+import withErrorHandler from '../../hoc/withErrorHandler/withError'
+import Spinner from '../../components/UI/Spinner/Spinner';
 import classes from './Content.module.css'
 
 class Content extends Component {
 
     state = {
         pizzas: [],
+        error: null,
         loader: true
     }
 
     componentDidMount() {
-        axios.get('/search?q=pizza').then(response => {
-            const pizzas = response.data.recipes.slice(0, 2);
-            console.log(pizzas)
-            this.setState({pizzas: pizzas, loader: false});
-        });
-    }
+        axios.get('/search?q=pizza')
+            .then(response => {
+                const pizzas = response.data.recipes.slice(0, 2);
+                console.log(pizzas)
+                this.setState({pizzas: pizzas, loader: false})
+            })
+            .catch(error => {
+                console.log(error);
+                this.setState({error: error})
+            });
+        };
 
     render() {
 
+        let cards = this.state.error ? null : <div className={classes.spinner}><Spinner/></div> 
+
+        if(!this.state.loader) {
+            cards = this.state.pizzas.map(obj => <Card key={obj.recipe_id} title={obj.title} img={obj.image_url} publisher={obj.publisher}></Card>)
+        } 
 
         return (
             <div className={classes.Content}>
-                {this.state.pizzas.map(obj => <Card key={obj.recipe_id} title={obj.title} img={obj.image_url} publisher={obj.publisher}></Card>)}
-                {this.state.loader ? <div className={classes.spinner}><Spinner/></div> : null}
+                {cards}
             </div>
         )
     }
 
 }
 
-export default Content;
+export default withErrorHandler(Content, axios);
